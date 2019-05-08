c78d118473053eb40e42934bb9809a4c44bd38a6
Gennady Glushenkov <gennadiyglushenkov@gmail.com>
2019-05-08 10:18:12 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {Component} from "react"
 
-
-function App() {
-  return (
-    <div className="App">
-      hello40
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            loading: false,
+            character: {}
+        }
+    }
+    
+    componentDidMount() {
+        this.setState({loading: true})
+        fetch("https://swapi.co/api/people/1")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    loading: false,
+                    character: data
+                })
+            })
+    }
+    
+    render() {
+        const text = this.state.loading ? "loading..." : this.state.character.name
+        return (
+            <div>
+                <p>{text}</p>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
